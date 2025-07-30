50ca2581b0899256df9ddddfb636db0e108a6f46
Brian Mulloy <brian@landlessness.net>
2025-07-29 20:14:24 -0400

    titles

--- R/mami.codi.beaty.R
+++ R/mami.codi.beaty.R
@@ -157,8 +157,7 @@ compute_time_cycles <- function(
 ) {
 
   frequency_spectrum = validate_combine_spectra(
-    x$stimulus_frequency_spectrum[[1]],
-    x$cubic_distortion_frequency_spectrum[[1]]
+    x$stimulus_frequency_spectrum[[1]]
   )
 
   f_min <- min(x$stimulus_frequency_spectrum[[1]]$frequency)
